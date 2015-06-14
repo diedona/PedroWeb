99302c67481ff760463322f1bc4dfeb6cbc3d6cc
diedona <diedona@gmail.com>
2015-06-14 15:21:16 -0300

    formulário para inserção de frase

--- dev/scripts/controllers/RoletaController.js
+++ dev/scripts/controllers/RoletaController.js
@@ -1,8 +1,9 @@
 (function () {
     var app = angular.module('pedro-web.controllers.roleta', []);
     app.controller('RoletaController', [
-        '$scope', '$log', '$rootScope', '$stateParams', 'CtrlOptions',
-        function ($scope, $log, $rootScope, $stateParams, CtrlOptions) {
+        '$scope', '$log', '$rootScope', '$stateParams', '$location',
+        'CtrlOptions',
+        function ($scope, $log, $rootScope, $stateParams, $location, CtrlOptions) {
 
             $rootScope.activeMenu = "roleta";
 
@@ -12,9 +13,21 @@
             if (CtrlOptions.acao == "listar-roletas") {
                 //listar roletas
             } else if (CtrlOptions.acao == "listar-frases") {
-                //listar as frases
+                $scope.roleta = $stateParams.nome;
             } else if (CtrlOptions.acao == "adicionar-frases") {
                 //preparar form para inserir
+                $scope.frase = {};
+                $scope.frase.roleta = $stateParams.nome;
+            }
+            
+            $scope.SalvarFrase = function(frmFrase, frase) {
+                
+                if(!frmFrase.$valid) {
+                    return;
+                }
+                
+                $location.path("/roleta/" + frase.roleta);
+                
             }
         }
     ]);
